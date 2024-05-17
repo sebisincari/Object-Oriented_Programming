6dc2b6edb260b4c8637534ab6b232cb7f4ebe57c
Sincari Sebastian <sincarisebastian@gmail.com>
2024-05-18 02:18:43 +0300

    al doilea

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<variant>
+#include<cmath>
+#include<queue>
+#include<unordered_map>
 using namespace std;
 
 class coordonate
@@ -20,27 +28,43 @@ class graphStrategy{
 };
 
 class unorientedGraphStrategy: public graphStrategy{
+    private:
+        vector<vector<pair<double,int>>> graf;//stocheaza muchiile cu tot cu distanta
+        string regionName;
+        vector<string> citiesName;
+        unordered_map<string,int> cityIndex;//un hashmap pentru rapiditatea gasirii indicelui
+        int numberOfCities;
+        int findIndex(string cityName);//de verificat daca se mosteneste 
     public:
         void addRoad(string city1, string city2, coordonate a,coordonate b) override;
         void getDistance(string city1, string city2) override;
+        friend istream& operator>>(istream &in, coordonate &c);
 };
 
 class map{
     private:
-        graphStrategy *strategy;
+        graphStrategy **strategy;
     public:
-        map(graphStrategy *strategy) : strategy(strategy) {}
+        map(graphStrategy **strategy) : strategy(strategy) {}
         map(const map &other);
         ~map();
         void operator=(const map &other);
-        friend istream& operator>>(istream &in, coordonate &c);
+        void setStrategy(graphStrategy *strategy);
+        void readCity();
+        void readRoads();
+        void respondQueries();
         
         
 };
-
+//citim operatia
+//1 --> harta->readCity();
+//2 --> harta->readRoads();
+//3 --> harta->respondQueries();
+// in harta->readCity() se citeste numele si tipul de graf
+// in harta->readRoads() se citesc oras coord oras coord regiune
 int main()
 {
-    graphStrategy *graph_strategy = new unorientedGraphStrategy();
+    graphStrategy **graph_strategy = new graphStrategy*[10001];
     map harta(graph_strategy);
     return 0;
 }
